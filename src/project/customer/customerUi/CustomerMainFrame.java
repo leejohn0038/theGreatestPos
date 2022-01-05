b9175d476498fb10e87e1663f0351525e9b81d93
coldp <coldp@DESKTOP-BPIQT61>
2022-01-05 11:30:03 +0900

    del

--- src/project/customer/customerUi/CustomerMainFrame.java
+++ src/project/customer/customerUi/CustomerMainFrame.java
@@ -9,11 +9,7 @@ public class CustomerMainFrame extends JFrame {
 	public CustomerMainFrame() {
 		new CustomerInputButton();
 		
-		String[] CIbtn = {"ID : ", "이름 : ", "휴대폰 번호 : ", "주소 : ", "가입일자 : ", "멤버십 포인트 : "};
-
-		for (int i = 0; i < CIbtn.length; ++i) {
-			add(new CustomerInputButtonUi(CIbtn[i]));
-		}
+		
 
 	}
 	public static void main(String[] args) {
