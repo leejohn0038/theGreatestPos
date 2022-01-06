ad127afeac4de042c73f9ae3f964623a61a8d9f4
Gi7D-02 <Gi7D-02@Gi7D-20>
2022-01-06 09:43:35 +0900

    시험을 위해 GoodsMain에 정답용 문장을 추가함

--- src/project/GoodsMain.java
+++ src/project/GoodsMain.java
@@ -2,9 +2,11 @@ package project;
 
 import project.frames.goods_frames.GoodsFrame;
 
-public class Goods_main {
+public class GoodsMain {
 
 	public static void main(String[] args) {
 		new GoodsFrame();
+		
+		String answer = "This sentence is for the test";
 	}
 }
