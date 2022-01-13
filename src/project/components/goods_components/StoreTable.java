0e184bd5d17495884dce6d4995fe653354011908
Gi7D-02 <Gi7D-02@Gi7D-20>
2022-01-13 09:30:56 +0900

    입고버튼 기능 구현중

--- src/project/components/goods_components/StoreTable.java
+++ src/project/components/goods_components/StoreTable.java
@@ -19,34 +19,39 @@ public class StoreTable extends JPanel {
 	private JTable table;
 	private JScrollPane sp;
 	
+	String name;
 	
 	public StoreTable() {
 		
+		try (
+			Connection conn = PosDBConnector.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM gstore INNER JOIN goods USING (gname)");
+			ResultSet rs = pstmt.executeQuery();
+		) {
+			int i = 0;
+			while (rs.next()) {
+				
+				name = rs.getString("gname");
+				rowData[0][i] = name;
+				i++;
+			}
+			
+			
+			model = new DefaultTableModel(rowData, column_name);
+			table = new JTable(model);
+			sp = new JScrollPane(table);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 		setLayout(new BorderLayout());
-		model = new DefaultTableModel(rowData(), column_name);
-		table = new JTable(model);
-		sp = new JScrollPane(table);
 		
 		sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		add(sp);
 		setBounds(0, 50, 650, 400);
 		setVisible(true);
-	}
-	
-	private ResultSet rs() {
-		try (
-			Connection conn = PosDBConnector.getConnection();		
-			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM gstore INNER JOIN goods USING (gname)");
-			ResultSet rs = pstmt.executeQuery();
-		) {
-						
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return rs;
 	}
 	
 	public DefaultTableModel getTableModel() {
